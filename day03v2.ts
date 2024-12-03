4d302b55160f28406d0710a1776628f74b3c640f
Jonas Theslöf <theslof@gmail.com>
2024-12-03 21:08:18 +0100

    Refactor day03v2.ts

--- day03v2.ts
+++ day03v2.ts
@@ -13,19 +13,19 @@ export function part1(): number {
 export function part2(): number {
   let input = readData(3).join('')
 
-  const regex = /do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\)/g
-
   let multiply = 1
   let sum = 0
-  let match
-  while ((match = regex.exec(input)) !== null) {
+  for (const match of input.matchAll(/do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\)/g)) {
     const [pattern, first, second] = match
-    if (pattern.startsWith('mul(')) {
-      sum += Number.parseInt(first) * Number.parseInt(second) * multiply
-    } else if (pattern.startsWith('do(')) {
-      multiply = 1
-    } else {
-      multiply = 0
+    switch (pattern) {
+      case 'do()':
+        multiply = 1
+        break
+      case "don't()":
+        multiply = 0
+        break
+      default:
+        sum += Number.parseInt(first) * Number.parseInt(second) * multiply
     }
   }
 
